dc33af2d84cdf353197a7ae3f43f7b69c5ff93fe
liunian-Jay <jiangyijcx@163.com>
2024-12-26 16:05:28 +0800

    后端test

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 进入frontend项目，运行**npm install**，安装**node_modules**依赖包
 
 # 后端项目
-环境依赖是：. . . 
+#### 环境
+conda create --name EduSail python=3.10
+python -m pip install --upgrade pip
+
+Flask==3.1.0
--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,18 +1,16 @@
 from flask import Blueprint, request, jsonify
-from app.services.inference_service import InferenceService
 
-model_routes = Blueprint('model_routes', __name__)
+test = Blueprint('test', __name__)
 
-@model_routes.route('/predict', methods=['POST'])
-def predict():
+@test.route('/test', methods=['POST'])
+def test():
     try:
         # 从请求中获取数据
         data = request.get_json()
-        model_name = data.get('model_name')
-        inputs = data.get('inputs')
+        test = data.get('test')
 
         # 调用推理服务
-        result = {"model_name": model_name, "prediction": result}
+        result = {"res": test, "prediction": '你好'}
         return jsonify(result), 200
     except Exception as e:
         return jsonify(error=str(e)), 500
